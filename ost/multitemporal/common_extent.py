116b96c5576438aff78b7022925dd10b24e2d50b
JamesWheeler <jamesemwheeler@gmail.com>
2020-01-07 15:19:05 +0100

    Fixed some bugs for coherence only product type

--- ost/multitemporal/common_extent.py
+++ ost/multitemporal/common_extent.py
@@ -18,7 +18,6 @@ def mt_extent(list_of_scenes, out_file, temp_dir, buffer=None):
     gdal.BuildVRT(opj(out_dir, 'extent.vrt'),
                   list_of_scenes,
                   options=vrt_options)
-
     start = time.time()
 
     outline_file = opj(temp_dir, os.path.basename(out_file))
