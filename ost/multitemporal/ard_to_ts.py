04bd0764cca4d4624cce504a8405e83969590c4b
JamesWheeler <jamesemwheeler@gmail.com>
2020-01-16 14:31:41 +0100

    Sorting out power to dB bug for timeseries

--- ost/multitemporal/ard_to_ts.py
+++ ost/multitemporal/ard_to_ts.py
@@ -129,7 +129,7 @@ def ard_to_ts(list_of_files, processing_dir, temp_dir,
             ard_mt_speck = ard_params['time-series ARD']['mt speckle filter']
     # get the db scaling right
     to_db = ard['to db']
-    if to_db or product is not 'bs':
+    if to_db or product != 'bs':
         to_db = False
         print('Not converting to dB for {}'.format(product))
     else:
