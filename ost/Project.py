2da94929b4ae85bec99d32b6aac44c0fe0b7a301
JamesWheeler <jamesemwheeler@gmail.com>
2020-01-17 15:55:21 +0100

    Added name prefixing to timeseries animations

--- ost/Project.py
+++ ost/Project.py
@@ -594,7 +594,7 @@ class Sentinel1_SLCBatch(Sentinel1):
         
         ras.create_timeseries_animation(timeseries_dir, product_list, outfile, 
                                     shrink_factor=shrink_factor, duration=duration, resampling_factor=resampling_factor,
-                                    add_dates=add_dates,prefix=prefix)
+                                    add_dates=add_dates, prefix=prefix)
     def multiprocess(self, timeseries=False, timescan=False, mosaic=False,
                      overwrite=False, exec_file=None, cut_to_aoi=False, ncores=os.cpu_count(), multiproc=os.cpu_count()):
         '''
